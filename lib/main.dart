1498971a861dc6df46fbd8aade6531bbd321b8a0
Mercuto <manushakya.vb@gmail.com>
2020-03-15 20:05:28 +0530

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:blood_bank/Screens/login_screen.dart';
 import 'package:blood_bank/Screens/main_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+
 
 void main() => runApp(MyApp());
 
